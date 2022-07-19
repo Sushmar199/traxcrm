a2355b788d502bcef69b5b89782660d4358f0e0a
Sushmar <sushmar1009@gmail.com>
2022-07-19 09:58:33 +0530

    traxdb version1.0

---
+++ src/main/java/com/traxcrm/service/LeadService.java
@@ -0,0 +1,13 @@
+package com.traxcrm.service;
+
+import java.util.List;
+
+import com.traxcrm.entities.Lead;
+
+public interface LeadService {
+	public void saveLead(Lead lead);
+	public Lead getLeadById(long id);
+	public void deleteOneLead(long id);
+	public List<Lead> listAll();
+	
+}
